f1b32dba9777388ab9c6db665c90b0f9c482fe58
Scott Yih <scottyih@allenai.org>
2018-11-08 13:54:02 -0800

    add quac poster

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -25,7 +25,7 @@ import pandas as pd
 # 
 # Pandas makes this easy with the read_excel function. 
 # 
-publications = pd.read_excel("publications.xls", sheetname='main')
+publications = pd.read_excel("publications.xlsx", sheetname='main')
 
 
 # ## Escape special characters
